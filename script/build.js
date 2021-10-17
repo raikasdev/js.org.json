dc4b5e2abf2827450730ceaebab1e8270b1bb1a3
Raikas <roni.aikas@gmail.com>
2021-10-17 14:29:39 +0300

    Add pretty print

--- script/build.js
+++ script/build.js
@@ -26,6 +26,6 @@ function download(url, dest, callback) {
 download('https://raw.githubusercontent.com/js-org/js.org/master/cnames_active.js', 'tmp_data.js', () => {
     fs.appendFileSync('tmp_data.js', '\nexports.cnames_active = cnames_active;');
     const data = require('../tmp_data.js')
-    fs.writeFileSync('output/cnames_active.json', JSON.stringify(data.cnames_active));
-    fs.writeFileSync('output/subdomains_registered.json', JSON.stringify(Object.keys(data.cnames_active).map(value => `${value}.js.org`)));
+    fs.writeFileSync('output/cnames_active.json', JSON.stringify(data.cnames_active, null, 2));
+    fs.writeFileSync('output/subdomains_registered.json', JSON.stringify(Object.keys(data.cnames_active).map(value => `${value}.js.org`), null, 2));
 })
